a448e2531eff2bc6f5bb2481331a7957dac912b8
Biblenatore <biblenatore@gmail.com>
2025-09-08 14:33:10 -0700

    improve syntax

--- merge-conflicts.js
+++ merge-conflicts.js
@@ -1,11 +1,7 @@
 // this function adds 2 nums
-const add = (n1, n2) => {
-  return n1 + n2;
-};
+const add = (n1, n2) => n1 + n2;
 
-const subtract = (n1, n2) => {
-  return n1 - n2
-}
+const subtract = (n1, n2) => n1 - n2;
 
 export {
   add,
